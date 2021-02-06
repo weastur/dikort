dcbecf0addc7c3ff1c979323875e918fbdc57fbf
Pavel Sapezhko <me@weastur.com>
2021-02-06 19:51:10 +0300

    Initial config

---
+++ dikort/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, validator
+import typing
+
+
+class Rules(BaseModel):
+    max_length: int
+
+    @validator('max_length')
+    def max_length_min(self, value):
+        if value < 1:
+            raise ValueError('max_length must be greater than 0')
+        return value
+
+
+class Config(BaseModel):
+
+    default_rules: Rules
---
+++ hooks/lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+black --check dikort
+#flake8 votion
+#mypy votion
