df0b5fc32c9f63d376ec1465956e5399313ef5c1
Pavel Sapezhko <me@weastur.com>
2021-03-11 11:16:21 +0300

    Add defaults

--- dikort/config.py
+++ dikort/config.py
@@ -2,8 +2,20 @@ import configparser
 import logging
 
 
+DEFAULTS = {
+    "logging": {
+        "enabled": False,
+        "format": "%%(levelname)s - %%(asctime)s - %%(filename)s:%%(lineno)d - %%(message)s",
+        "datefmt": "%%Y-%%m-%%d %%H:%%M:%%S",
+        "level": "INFO",
+        "another": True,
+    }
+}
+
+
 def parse(args):
     config = configparser.ConfigParser()
+    config.read_dict(DEFAULTS)
     config.read(args["config"])
     return config
 
