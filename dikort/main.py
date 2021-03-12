14b53305128596fbabbe3cfe45ec4162d9c27508
Pavel Sapezhko <me@weastur.com>
2021-03-12 10:28:56 +0300

    Add checking for new version

--- dikort/main.py
+++ dikort/main.py
@@ -1,12 +1,39 @@
 import argparse
+import json
+import logging
+import urllib.error
+import urllib.request
 
 import colorama
 
+import dikort
 from dikort.config import configure_logging, parse
+from dikort.print import print_warning
+
+GITHUB_RELEASES_API_URL = "https://api.github.com/repos/weastur/dikort/releases"
+
+
+def check_for_new_version():
+    try:
+        with urllib.request.urlopen(GITHUB_RELEASES_API_URL, timeout=1) as resp:
+            releases = json.loads(resp.read())
+    except urllib.error.URLError as err:
+        logging.error("Cannot check for a new version, error: %s", err)
+        return
+    tags = [release["tag_name"] for release in releases]
+    if not tags:
+        return
+    tags.sort()
+    latest_tag = tags[-1][1:]
+    if dikort.__version__ < latest_tag:
+        print_warning(
+            f"There is a new version: {latest_tag}. Please, consider to update"
+        )
 
 
 def main():
     colorama.init()
+    check_for_new_version()
     cmd_args_parser = argparse.ArgumentParser(
         prog="dikort", description="Commit messages checking tool"
     )
--- dikort/print.py
+++ dikort/print.py
@@ -4,3 +4,8 @@ from colorama import Fore
 def print_error(*args, sep=" ", end="\n"):
     line = sep.join(args)
     print(f"{Fore.RED}{line}{Fore.RESET}", end=end)
+
+
+def print_warning(*args, sep=" ", end="\n"):
+    line = sep.join(args)
+    print(f"{Fore.YELLOW}{line}{Fore.RESET}", end=end)
