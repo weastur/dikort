b144d03db517f98da813bf3d3976111e899e96a4
Pavel Sapezhko <me@weastur.com>
2021-03-16 16:40:35 +0300

    Fix filters test

--- dikort/tests/test_filters.py
+++ dikort/tests/test_filters.py
@@ -1,4 +1,4 @@
-import configparser
+import re
 from unittest import TestCase
 from unittest.mock import Mock
 
@@ -18,100 +18,99 @@ from dikort.filters import (
 class TestFilters(TestCase):
     def setUp(self):
         self.commit = Mock()
-        self.config = configparser.ConfigParser(interpolation=None)
-        self.config.add_section("rules.settings")
+        self.config = {}
 
     def test_singleline(self):
         self.commit.summary = "Singleline summary\n"
-        self.config["rules.settings"]["singleline-summary"] = "yes"
+        self.config["singleline_summary"] = True
         self.assertFalse(filter_singleline(self.commit, config=self.config))
-        self.config["rules.settings"]["singleline-summary"] = "no"
+        self.config["singleline_summary"] = False
         self.assertTrue(filter_singleline(self.commit, config=self.config))
 
         self.commit.summary = "Multiline summary\nAnother line\n"
-        self.config["rules.settings"]["singleline-summary"] = "yes"
+        self.config["singleline_summary"] = True
         self.assertTrue(filter_singleline(self.commit, config=self.config))
-        self.config["rules.settings"]["singleline-summary"] = "no"
+        self.config["singleline_summary"] = False
         self.assertFalse(filter_singleline(self.commit, config=self.config))
 
     def test_trailing_period(self):
         self.commit.summary = "No trailing period summary\n"
-        self.config["rules.settings"]["trailing-period"] = "no"
+        self.config["trailing_period"] = False
         self.assertFalse(
             filter_trailing_period(self.commit, config=self.config)
         )
-        self.config["rules.settings"]["trailing-period"] = "yes"
+        self.config["trailing_period"] = True
         self.assertTrue(filter_trailing_period(self.commit, config=self.config))
 
         self.commit.summary = "Trailing period summary\n"
-        self.config["rules.settings"]["trailing-period"] = "no"
+        self.config["trailing_period"] = False
         self.assertFalse(
             filter_trailing_period(self.commit, config=self.config)
         )
-        self.config["rules.settings"]["trailing-period"] = "yes"
+        self.config["trailing_period"] = True
         self.assertTrue(filter_trailing_period(self.commit, config=self.config))
 
     def test_capitalized_summary(self):
         self.commit.summary = "Capitalized summary\n"
-        self.config["rules.settings"]["capitalized-summary"] = "yes"
+        self.config["capitalized_summary"] = True
         self.assertFalse(filter_capitalized(self.commit, config=self.config))
-        self.config["rules.settings"]["capitalized-summary"] = "no"
+        self.config["capitalized_summary"] = False
         self.assertTrue(filter_capitalized(self.commit, config=self.config))
 
         self.commit.summary = "not capitalized summary\n"
-        self.config["rules.settings"]["capitalized-summary"] = "no"
+        self.config["capitalized_summary"] = False
         self.assertFalse(filter_capitalized(self.commit, config=self.config))
-        self.config["rules.settings"]["capitalized-summary"] = "yes"
+        self.config["capitalized_summary"] = True
         self.assertTrue(filter_capitalized(self.commit, config=self.config))
 
         self.commit.summary = "[special case] not capitalized summary\n"
-        self.config["rules.settings"]["capitalized-summary"] = "yes"
+        self.config["capitalized_summary"] = True
         self.assertFalse(filter_capitalized(self.commit, config=self.config))
 
     def test_lentgh(self):
         self.commit.summary = 5 * "A"
-        self.config["rules.settings"]["max-length"] = "100"
+        self.config["max_length"] = 100
 
-        self.config["rules.settings"]["min-length"] = "10"
+        self.config["min_length"] = 10
         self.assertTrue(filter_length(self.commit, config=self.config))
-        self.config["rules.settings"]["min-length"] = "4"
+        self.config["min_length"] = 4
         self.assertFalse(filter_length(self.commit, config=self.config))
 
-        self.config["rules.settings"]["max-length"] = "4"
+        self.config["max_length"] = 4
         self.assertTrue(filter_length(self.commit, config=self.config))
-        self.config["rules.settings"]["max-length"] = "5"
+        self.config["max_length"] = 5
         self.assertFalse(filter_length(self.commit, config=self.config))
 
     def test_signoff(self):
         self.commit.message = (
             "My Awesome commit\n" "Fixed all bugs.\n" "Signed-off-by: Neo\n"
         )
-        self.config["rules.settings"]["signoff"] = "yes"
+        self.config["signoff"] = True
         self.assertFalse(filter_signoff(self.commit, config=self.config))
-        self.config["rules.settings"]["signoff"] = "no"
+        self.config["signoff"] = False
         self.assertTrue(filter_signoff(self.commit, config=self.config))
 
         self.commit.message = "My Awesome commit\n" "Fixed all bugs.\n"
-        self.config["rules.settings"]["signoff"] = "no"
+        self.config["signoff"] = False
         self.assertFalse(filter_signoff(self.commit, config=self.config))
-        self.config["rules.settings"]["signoff"] = "yes"
+        self.config["signoff"] = True
         self.assertTrue(filter_signoff(self.commit, config=self.config))
 
     def test_gpg(self):
         self.commit.gpgsig = "GPG-SIGNATURE"
-        self.config["rules.settings"]["gpg"] = "yes"
+        self.config["gpg"] = True
         self.assertFalse(filter_gpg(self.commit, config=self.config))
-        self.config["rules.settings"]["gpg"] = "no"
+        self.config["gpg"] = False
         self.assertTrue(filter_gpg(self.commit, config=self.config))
 
         self.commit.gpgsig = ""
-        self.config["rules.settings"]["gpg"] = "no"
+        self.config["gpg"] = False
         self.assertFalse(filter_gpg(self.commit, config=self.config))
-        self.config["rules.settings"]["gpg"] = "yes"
+        self.config["gpg"] = True
         self.assertTrue(filter_gpg(self.commit, config=self.config))
 
     def test_regex(self):
-        self.config["rules.settings"]["regex"] = r"DEV-\d+: \w+"
+        self.config["regex"] = re.compile(r"DEV-\d+: \w+")
 
         self.commit.summary = "DEV-123: my super fixes"
         self.assertFalse(filter_regex(self.commit, config=self.config))
@@ -119,7 +118,7 @@ class TestFilters(TestCase):
         self.assertTrue(filter_regex(self.commit, config=self.config))
 
     def test_author_name_regex(self):
-        self.config["rules.settings"]["author-name-regex"] = r"\w+ \w+"
+        self.config["author_name_regex"] = re.compile(r"\w+ \w+")
 
         self.commit.author.name = "Pavel Sapezhko"
         self.assertFalse(
@@ -131,7 +130,7 @@ class TestFilters(TestCase):
         )
 
     def test_author_email_regex(self):
-        self.config["rules.settings"]["author-email-regex"] = r"\w+@example.com"
+        self.config["author_email_regex"] = re.compile(r"\w+@example.com")
 
         self.commit.author.email = "neo@example.com"
         self.assertFalse(
